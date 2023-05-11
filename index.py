6c5cd16db8afe429936c739af4024ef66f94c48d
chargecyber <edomkyc777@gmail.com>
2023-05-12 03:28:55 +0900

    time consuming

--- index.py
+++ index.py
@@ -1,93 +1,95 @@
 from flask import Flask, request, jsonify
-import os
-import sys
-import config
 from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, LLMPredictor, PromptHelper, ServiceContext, StorageContext, load_index_from_storage
-from llama_index.optimization.optimizer import SentenceEmbeddingOptimizer
+# from llama_index.node_parser import SimpleNodeParser
 from langchain import OpenAI
-from flask_caching import Cache
+
+import os
+import config
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 60*60})
 
-# Load training data
-def load_document():
-    return SimpleDirectoryReader('data').load_data()
-
-# Initialize the index and storage context
-service_context = None
+# initialize index
 index = None
-storage_context = None
+
+@app.before_first_request
+def startup():
+    initialize_index()
 
 def initialize_index():
-    global service_context, index, storage_context
-
+    global index
+    # set OPENAI_API_KEY
     os.environ['OPENAI_API_KEY'] = config.OPENAI_API_KEY
 
+    # load training data from directory {root}/data/*
+    documents = SimpleDirectoryReader('data').load_data()
+
+    # parse the document into nodes
+    # parser = SimpleNodeParser()
+    # nodes = parser.get_nodes_from_documents(documents)
+
+    # index construction
+    # index = GPTVectorStoreIndex.from_documents(documents)
+    # index = GPTVectorStoreIndex(nodes)
+
+    # define LLM
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
 
+    # define prompt helper
+    # set maximum input size
     max_input_size = 4096
+    # set number of output tokens
     num_output = 256
+    # set maximum chunk overlap
     max_chunk_overlap = 20
     prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
 
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
-    documents = load_document()
+    index = GPTVectorStoreIndex.from_documents(
+        documents, service_context=service_context
+    )
 
-    index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+    # save the index for future use - save ./storage as default
     index.storage_context.persist()
 
+    # rebuild storage context
     storage_context = StorageContext.from_defaults(persist_dir="storage")
-    index = load_index_from_storage(storage_context, service_context=service_context)
 
-# call initialize_index function when app starts up
-@app.before_first_request
-async def startup():
-    initialize_index()
+    # load index
+    index = load_index_from_storage(
+        storage_context, service_context=service_context
+    )
 
 @app.route('/')
 def home():
     return 'Server is running.'
 
 @app.route('/webhook', methods=['POST'])
-async def webhook():
-    
+def webhook():
     try:
         # get parameter
         data = request.get_json()
         query_result = data.get('queryResult')
         query = query_result.get('queryText')
-        
+
         if query_result.get('action') == 'input.unknown':
-            # use cache to store query responses
-            response = cache.get(query)
-            if response is None:
-                response = await query_gpt(query)
-                cache.set(query, response)
+            result = query_gpt(query)
 
         if query_result.get('action') == 'welcome':
-            response = "Hi, I am a custom service assistant."
-
-        result = {
-            'fulfillmentText': response
+            result = "Hi, I am a custom service assistant."
+        
+        response = {
+            'fulfillmentText': result
         }
-        return jsonify(result)
+        return jsonify(response)
     
     except Exception as e:
-        print('error', e)
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print('oops', exc_type, fname, exc_tb.tb_lineno)
-        return '', 400
-
-async def query_gpt(query):
+        return jsonify(e), 400
+    
+def query_gpt(query):
     global index
-
-    # high-level API - query the index
-    query_engine = index.as_query_engine(
-        optimizer=SentenceEmbeddingOptimizer(percentile_cutoff=0.5)
-    )
+    # high level API - query the index
+    query_engine = index.as_query_engine()
 
     return query_engine.query(query).response.strip('\n')
 
